f834294a0a38ab9b3c6da06ff74b54f68464c659
GUOXIAOLEI <47165252+pain2gain@users.noreply.github.com>
2019-09-09 17:27:10 +0200

    chatbot

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ const AssistantV1 = require('ibm-watson/assistant/v1');
 const service = new AssistantV1({
   version: '2019-02-28',
   username: "apikey",
-  iam_apikey: '#your_api_key', // fill in your APIKey
-  url: '#url_of_your_region'  // fill in your URL, in France https://gateway-lon.watsonplatform.net/assistant/api
+  iam_apikey: 'pcvYiiqZJfNybunAe3hgensVK2W8KWULc2wbJ3NQFX1a', // fill in your APIKey
+  url: 'https://gateway-fra.watsonplatform.net/assistant/api'  // fill in your URL, in France https://gateway-lon.watsonplatform.net/assistant/api
 });
 
 app.post('/sendMsg',function(req,res,err){
   console.log("Msg: "+req.body.jsonDatas);
   service.message({                       // Send the Msg to the chatbot, according the API of IBM Watson
-  workspace_id: '#your_workplace_id',   // Fill your workplace_id
+  workspace_id: '0ccb537a-4f1a-40f2-aa79-cd2d3a2141e8',   // Fill your workplace_id
   input: {'text': req.body.jsonDatas}
   }).then(response => {
       var responseJson = JSON.parse(JSON.stringify(response, null, 2));
@@ -263,5 +263,5 @@ var jsonDI =JSON.stringify(textJson,null, 2);
 
 //listening the port of 3001
 app.listen(3001,()=>{
-    console.log("http://localhost:3001");
+    console.log("listening to port: http://localhost:3001");
   })
